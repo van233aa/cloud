9c7efe0a94b90b1e597a1de0e017bc0a23886891
van233aa <147528270+van233aa@users.noreply.github.com>
2024-01-25 20:54:28 +0800

    Update 获取微博评论数据.py

--- 获取微博评论数据.py
+++ 获取微博评论数据.py
@@ -8,9 +8,9 @@ csv_writer.writerow(['id','screen_name','text_raw','source','like_counts','follo
 
 headers={
 
-    'Cookie':'SINAGLOBAL=3623229557425.0317.1625324086810; UOR=,,cn.bing.com; PC_TOKEN=b5b2fa254f; login_sid_t=ba60f9e75d6940e2edbeb783a9c70410; cross_origin_proto=SSL; _s_tentry=cn.bing.com; Apache=8362608304459.196.1704271259267; ULV=1704271259270:15:2:2:8362608304459.196.1704271259267:1704202651947; XSRF-TOKEN=yogNKDdT839ntf1iLbPKGdWi; WBStorage=267ec170|undefined; wb_view_log=1536*8641.25; crossidccode=CODE-gz-1RkWOP-3mNQxo-dzgyiCvIV8hUGVj4c93c0; SSOLoginState=1704271326; SUB=_2A25IkW2ODeRhGeBH41QV9CbKyTmIHXVr7-9GrDV8PUJbkNAbLVnSkW1NQbEgcHhHcsayyZU5oiLH9027vHcTLnig; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9Wh1Jj_Ib3xXscpxvgD9kS255JpX5KzhUgL.Foq41hqXShnceo-2dJLoI0qLxKBLBonLB.2LxKBLB.zL1h5LxKBLBo.L1hnLxKqL1-eL1h.LxKBLB.2LB.2LxK-L1K2LBKzt; WBPSESS=P9Jsf0pjIDbGo9A5vjDc6WeRc5gJvkaPXwYfSHMWxrpDg2JSK7PwLLC1NoS9BefKIeyPH09mQlXWOXcZgn4eJgu7l5ni1sAF6Dphasq6Nz-_2GXzWN0YzTdKZJNLVHVPAiH33xVx3mRcuulC7satNA==',
-    'Referer':'https://weibo.com/1749127163/NA8tbCaao',
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0',
+    'Cookie':'.............................',
+    'Referer':'.......................................',
+    'User-Agent':'.........................................................',
 }
 def get_next(next='count=10'):
     url=f"https://weibo.com/ajax/statuses/buildComments?is_reload=1&id=4986123699095548&is_show_bulletin=2&is_mix=0&{next}&uid=1"
